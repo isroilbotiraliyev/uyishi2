b9f0edc6e9dad2cfeb3e39de17202bed06a96049
isroil <isroilbotiraliyev@email.com>
2023-02-26 21:47:41 +0500

    origin

---
+++ main.js
@@ -0,0 +1,8 @@
+var son1 = +prompt('1-son kiriting');
+var son2 = +prompt('2-son kiriting');
+var son3 = +prompt('3-son kiriting');
+
+natija = (son1 + son2 + son3) / 3;
+
+
+ alert("sonlarning o'rta qiymati " + natija +  ' teng ');
